69ede3ccb7124dea9873ade9bfdd6f324d842015
Maksim Ivanov <satansdeer@gmail.com>
2026-05-16 16:47:07 +0200

    Refine Timmy assistant callout

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const APP_CACHE_PREFIX = "timey-app-";
-const CACHE_NAME = `${APP_CACHE_PREFIX}v31`;
+const CACHE_NAME = `${APP_CACHE_PREFIX}v32`;
 const LEGACY_APP_CACHE_PATTERN = /^timey-v\d+$/;
 const ASSETS = [
   "./",
