1f75ded30d2cb8197acae753a97d6027a517ab0e
Maksim Ivanov <satansdeer@gmail.com>
2026-05-16 16:41:14 +0200

    Make inactive tab line visible

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const APP_CACHE_PREFIX = "timey-app-";
-const CACHE_NAME = `${APP_CACHE_PREFIX}v30`;
+const CACHE_NAME = `${APP_CACHE_PREFIX}v31`;
 const LEGACY_APP_CACHE_PATTERN = /^timey-v\d+$/;
 const ASSETS = [
   "./",
